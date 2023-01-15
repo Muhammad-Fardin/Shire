4a5a05f6a95165e1c69843947b1746e6e71d2795
Muhammad Fardin <fardinbmw5@gmail.com>
2023-01-15 20:28:57 +0530

    Update FriendListWidget.jsx

--- client/src/components/FriendListWidget.jsx
+++ client/src/components/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `https://shire.onrender/users/${userId}/friends`,
+      `https://shire.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
