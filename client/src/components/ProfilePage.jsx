d6021ffb64dcf41102fc28b020d2d9869ef2e616
Muhammad Fardin <fardinbmw5@gmail.com>
2023-01-15 20:32:06 +0530

    Update ProfilePage.jsx

--- client/src/components/ProfilePage.jsx
+++ client/src/components/ProfilePage.jsx
@@ -15,7 +15,7 @@ const ProfilePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
   const getUser = async () => {
-    const response = await fetch(`https://shire.onrender/users/${userId}`, {
+    const response = await fetch(`https://shire.onrender.com/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
