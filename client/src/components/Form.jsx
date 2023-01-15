3892a546dee7fc130259a6125e6381668ff90f0d
Muhammad Fardin <fardinbmw5@gmail.com>
2023-01-15 20:28:24 +0530

    Update Form.jsx

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -63,7 +63,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://shire.onrender/auth/register",
+      "https://shire.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
@@ -78,7 +78,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("https://shire.onrender/auth/login", {
+    const loggedInResponse = await fetch("https://shire.onrender.com/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
