760b9bc84dd09b2dd849cae3ba4bf575e7058e89
Muhammad Fardin <fardinbmw5@gmail.com>
2023-01-15 20:32:25 +0530

    Update UserImage.jsx

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`https://shire.onrender/assets/${image}`}
+        src={`https://shire.onrender.com/assets/${image}`}
       />
     </Box>
   );
