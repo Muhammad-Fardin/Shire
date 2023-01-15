1de5e7d5dcf806b6a6aff83ea8580ba731ea2817
Muhammad Fardin <fardinbmw5@gmail.com>
2023-01-15 20:32:56 +0530

    Update UserWidget.jsx

--- client/src/components/UserWidget.jsx
+++ client/src/components/UserWidget.jsx
@@ -22,7 +22,7 @@ const UserWidget = ({ userId, picturePath }) => {
   const main = palette.neutral.main;
 
   const getUser = async () => {
-    const response = await fetch(`https://shire.onrender/users/${userId}`, {
+    const response = await fetch(`https://shire.onrender.com/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
